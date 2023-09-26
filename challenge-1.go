ba9e9970d8f434910ee5e9c3acb93a57d415893f
Natalie Till <natalieannatill@gmail.com>
2023-09-26 15:38:48 +0100

    First Challenge

--- challenge-1.go
+++ challenge-1.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
+// A list of airports
 var airports = []string{"PHX", "BKK", "OKC", "JFK", "LAX", "MEX", "EZE", "HEL", "LOS", "LAP", "LIM"}
 
+// A list of routes between airports
 var routes = [][2]string{
 	{"PHX", "LAX"},
 	{"PHX", "JFK"},
@@ -19,45 +17,5 @@ var routes = [][2]string{
 	{"LIM", "BKK"},
 }
 
-// Define a struct to represent the graph
-type Graph struct {
-	AdjacencyList map[string][]string
-}
-
-// Create a new graph
-func NewGraph() *Graph {
-	return &Graph{
-		AdjacencyList: make(map[string][]string),
-	}
-}
-
-// Add a node to the graph
-func (g *Graph) AddNode(airport string) {
-	g.AdjacencyList[airport] = []string{}
-}
-
-// Add an undirected edge to the graph
-func (g *Graph) AddEdge(origin, destination string) {
-	g.AdjacencyList[origin] = append(g.AdjacencyList[origin], destination)
-	g.AdjacencyList[destination] = append(g.AdjacencyList[destination], origin)
-}
-
-func main() {
-	// Create the graph
-	graph := NewGraph()
-
-	// Add nodes
-	for _, airport := range airports {
-		graph.AddNode(airport)
-	}
-
-	// Add edges
-	for _, route := range routes {
-		graph.AddEdge(route[0], route[1])
-	}
-
-	// Print the adjacency list
-	for airport, connections := range graph.AdjacencyList {
-		fmt.Printf("%s -> %v\n", airport, connections)
-	}
-}
+// Challenge 1.1: Represent the graph
+// Challenge 1.2: Use a search mthod to figure out if there exists a route between PHX and BKK
